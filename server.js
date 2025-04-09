1e1d56f704df14114e64cc6c51afca4072eebcff
Ronnie <ronaldlholt3@gmail.com>
2025-04-09 16:29:12 -0400

    more things added

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const potionRoutes = require('./Routes/potionRoutes');
 const messageRoutes = require('./Routes/messageRoutes');
 const cors = require('cors');
-
+require('dotenv').config()
 const app = express();
 const PORT = process.env.PORT || 5000; // Use a different port than your React app
 
@@ -14,8 +14,8 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-const mongoURI = 'mongodb+srv://ronaldlholt3:potions@potions.jhpzipu.mongodb.net/?retryWrites=true&w=majority&appName=Potions'; // **REPLACE THIS!**
-
+console.log(process.env.MONGODB_URI)
+const mongoURI=process.env.MONGODB_URI
 mongoose.connect(mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
